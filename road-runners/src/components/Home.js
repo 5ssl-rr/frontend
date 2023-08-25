d868f1e0a7c5f40e6aee0bb52cd5c5e92be344b8
Richard Perry <85124966+rdperry031@users.noreply.github.com>
2023-08-24 21:40:36 -0500

    added API call and state management to Home.js

--- road-runners/src/components/Home.js
+++ road-runners/src/components/Home.js
@@ -1,13 +1,40 @@
 import axiosWithAuth from '../utilities/axiosWithAuth';
 import Login from './Login';
 import AddressForm from './AddressForm';
-import Logout from './Logout';
-import { Link } from 'react-router-dom'; 
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
-    const token = localStorage.getItem('token')
-    return(
-       <><nav><Link to='/logout'>Logout</Link></nav>
-        <AddressForm/></>
-    )
+  const token = localStorage.getItem('token');
+  const [addresses, setAddresses] = useState([])
+  const [streets, setStreets] = useState([])
+  
+  useEffect(()=>{
+      axiosWithAuth()
+      .get('http://localhost:1447/api/address/')
+      .then((res) => {
+          setAddresses(res.data)
+          const streetList = [...new Set(res.data.map(addresses => addresses.street))]
+          setStreets(streetList)
+      })
+      .catch((err) => {
+          console.log(err)
+      })
+  }, [])
+
+  useEffect(() => {
+    console.log(addresses);
+    console.log(streets)
+  }, [addresses, streets]);
+
+
+  return (
+    <>
+      <nav>
+        <Link to='/logout'>Logout</Link>
+      </nav>
+<p></p>
+      <AddressForm />
+    </>
+  );
 }
