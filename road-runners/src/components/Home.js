93ae2eaf6472424d6ef4927ca326c2afad8d7796
Richard Perry <85124966+rdperry031@users.noreply.github.com>
2023-08-30 23:05:28 -0500

    address table now only displays address information for houses corresponding to selected street

--- road-runners/src/components/Home.js
+++ road-runners/src/components/Home.js
@@ -8,66 +8,20 @@ import Street from './Street';
 
 export default function Home() {
   const token = localStorage.getItem('token');
-  const [addresses, setAddresses] = useState([])
-  const [streets, setStreets] = useState([])
-  const [activeStreet, setActiveStreet] = useState();
   
-  useEffect(()=>{
-      axiosWithAuth()
-      .get('http://localhost:1447/api/address/')
-      .then((res) => {
-          setAddresses(res.data)
-          const streetList = [...new Set(res.data.map(addresses => addresses.street))]
-          setStreets(streetList)
-      })
-      .catch((err) => {
-          console.log(err)
-      })
-  }, [])
 
-  useEffect(() => {
-    console.log(addresses);
-    console.log(streets)
-  }, [addresses, streets]);
-
+  // useEffect(() => {
+  //   console.log(addresses);
+  //   console.log(streets);
+  // }, [addresses, streets]);
 
   return (
     <>
       <nav>
         <Link to='/logout'>Logout</Link>
       </nav>
-      <AddressForm/>
-      <div className='addresses-container'>
-      <table border="1">
-        <body>
-        <tr>
-            <th>House#</th>
-            <th>Delivery</th>
-          </tr>
-          {addresses.map((address) => (
-            <tr>
-              <td>{address.house}</td>
-              <td>{address.delivery}</td>
-            </tr>
-           ))}
-           
-        </body>
-      </table>
-      {/* {streets.map((street) => {
-        return(
-          <Street
-          street={street} streets={streets}/>
-        )
-      })} */}
-      {/* {addresses.map((address) => {
-        return(
-          <Address
-          address={address}
-          />
-        )
-      })} */}
-      
-      </div>
+      <AddressForm />
+     <Street/>
     </>
   );
 }
--- road-runners/src/components/Street.js
+++ road-runners/src/components/Street.js
@@ -1,16 +1,71 @@
-import { useState } from 'react';
-
-export default function Street(props) {
-  const { street, streets } = props;
+import { useState, useEffect } from 'react';
+import axiosWithAuth from '../utilities/axiosWithAuth';
+export default function Street() {
+  const [addresses, setAddresses] = useState([]);
+  const [streets, setStreets] = useState([]);
   const [activeStreet, setActiveStreet] = useState();
+  const [filteredAddresses, setFilteredAddresses] = useState([]);
 
+  useEffect(() => {
+    axiosWithAuth()
+      .get('http://localhost:1447/api/address/')
+      .then((res) => {
+        setAddresses(res.data);
+        const streetList = [
+          ...new Set(res.data.map((addresses) => addresses.street)),
+        ];
+        setStreets(streetList);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-  if (!street) {
+  useEffect(() => {
+    console.log(activeStreet);
+    console.log(addresses);
+  }, [activeStreet, addresses]);
+
+  useEffect(() => {
+    const filteredArray = addresses.filter(
+      (address) => address.street === activeStreet
+    );
+    setFilteredAddresses(filteredArray);
+  }, [addresses, activeStreet]);
+
+  if (!streets) {
     return <h3>failed to retrieve street information</h3>;
   }
   return (
     <>
-      <button>{street}</button>
+      <div className='addresses-container'>
+        <div className='street-container'>
+          <p>{activeStreet}</p>
+          {streets.map((street) => {
+            return (
+              <button key={street} onClick={() => setActiveStreet(street)}>
+                {street}
+              </button>
+            );
+          })}
+        </div>
+        <div className='address-table'>
+          <table border='1'>
+            <tbody>
+              <tr>
+                <th>House#</th>
+                <th>Delivery</th>
+              </tr>
+              {filteredAddresses.map((address) => (
+                <tr key={address.id}>
+                  <td>{address.house}</td>
+                  <td>{address.delivery}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </>
   );
 }
